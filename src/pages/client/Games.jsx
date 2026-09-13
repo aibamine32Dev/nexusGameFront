b0f1049c25baee60a8fe26166cfc5dcbf201a568
aibeche <aibecheamine8@gmail.com>
2026-09-13 21:34:27 +0100

    admin front  end  part

--- src/pages/client/Games.jsx
+++ src/pages/client/Games.jsx
@@ -1,20 +1,32 @@
+
 import { useState } from "react";
 import GameCard from "../../components/GameCard";
-import { games } from "../../data/games";
+import { games as initialGames } from "../../data/games";
 
 function Games() {
   const [filter, setFilter] = useState("ALL");
+
+  const savedGames = localStorage.getItem(
+    "nexusGames"
+  );
+
+  const games = savedGames
+    ? JSON.parse(savedGames)
+    : initialGames;
 
   const filteredGames =
     filter === "ALL"
       ? games
       : games.filter(
-          (game) => game.platform.toUpperCase() === filter
+          (game) =>
+            game.platform.toUpperCase() === filter
         );
 
   return (
     <section className="page-section">
+
       <div className="page-header">
+
         <span>THE LIBRARY</span>
 
         <h1>
@@ -27,27 +39,45 @@ function Games() {
           Choose your game. Choose your battlefield.
           Prepare to dominate.
         </p>
+
       </div>
 
       <div className="game-filters">
+
         {["ALL", "PC", "PS5"].map((item) => (
+
           <button
             key={item}
-            className={filter === item ? "active" : ""}
+            className={
+              filter === item
+                ? "active"
+                : ""
+            }
             onClick={() => setFilter(item)}
           >
             {item}
           </button>
+
         ))}
+
       </div>
 
       <div className="games-grid">
-        {filteredGames.map((game) => (
-          <GameCard key={game.id} game={game} />
-        ))}
+
+        {filteredGames
+          .filter((game) => game.available)
+          .map((game) => (
+            <GameCard
+              key={game.id}
+              game={game}
+            />
+          ))}
+
       </div>
+
     </section>
   );
 }
 
 export default Games;
+
